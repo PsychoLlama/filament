3ec90133a745cdfcbe08af47eaa41c2ecbb7ac3b
Jesse Gibson <Jesse_Gibson@me.com>
2017-05-07 10:15:28 -0600

    Add config
    Using the fabulous `rc` library, the port, host address, and graphiql
    config can be set externally.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import graphqlHttp from 'express-graphql';
 import express from 'express';
+import rc from 'rc';
 
 import * as fields from './fields';
 import schema from './schema';
 
-const graphqlEndpoint = graphqlHttp({
-  rootValue: fields,
+const config = rc('filament', {
+  host: '0.0.0.0',
   graphiql: true,
+  port: 8080,
+});
+
+const graphqlEndpoint = graphqlHttp({
+  graphiql: config.graphiql === true,
+  rootValue: fields,
   schema,
 });
 
 const app = express();
 app.use(graphqlEndpoint);
 
-app.listen(8080);
+app.listen(config.port, config.host);
