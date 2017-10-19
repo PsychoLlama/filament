5e321dbf0eeef87257dbc63762e3a18275f9db44
Mick Dekkers <mickdekkersnl@gmail.com>
2017-10-19 14:37:27 +0000

    Add mutation to set group name attribute

---
+++ src/resolvers/__tests__/set-group-attributes.test.js
@@ -0,0 +1,33 @@
+import { query, bridge, createGroup } from '../../test-utils';
+
+jest.mock('../../../bridge.json');
+
+describe('Group attributes mutation', () => {
+  let get, put, spy, group;
+
+  beforeEach(() => {
+    group = createGroup({ name: 'foo' });
+    spy = jest.fn(() => ({ fake: true }));
+
+    put = bridge.put('/groups/10').reply(200, spy);
+    get = bridge.get('/groups/10').reply(200, group);
+  });
+
+  afterEach(() => {
+    get.done();
+    put.done();
+  });
+
+  it('sets the group attributes', async () => {
+    await query`mutation {
+      hue {
+        setGroupAttributes(id: 10 attributes: { name: "bar" }) { name }
+      }
+    }`;
+
+    expect(spy).toHaveBeenCalled();
+    const [, json] = spy.mock.calls[0];
+
+    expect(JSON.parse(json)).toEqual({ name: 'bar' });
+  });
+});
---
+++ src/utils/__tests__/group-attributes-mutation.test.js
@@ -0,0 +1,23 @@
+import mutation from '../group-attributes-mutation';
+
+describe('Group attributes mutation', () => {
+  it('passes through unrecognized props', () => {
+    const data = { name: 'foo', potato: 'enabled', things: 1.28 };
+    const result = mutation(data);
+
+    expect(result).toEqual(data);
+  });
+
+  it('passes through unrecognized props when no recognized props are passed', () => {
+    const data = { potato: 'enabled', things: 1.28 };
+    const result = mutation(data);
+
+    expect(result).toEqual(data);
+  });
+
+  it('throws if the name is too long', () => {
+    const fail = () => mutation({ name: 'foozbarzfoozbarzfoozbarzfoozbarz1' });
+
+    expect(fail).toThrow(/name/i);
+  });
+});
