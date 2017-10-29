bf253c8a256a905ee7478d3c41ade2e6fb5024b3
Mick Dekkers <mickdekkersnl@gmail.com>
2017-10-29 12:51:35 +0100

    Refactor to remove unnecessary Object.assign

--- src/utils/light-attributes-mutation.js
+++ src/utils/light-attributes-mutation.js
@@ -1,13 +1,9 @@
 import assert from 'assert';
 
 export default mutation => {
-  const { name, ...patch } = mutation;
-
-  if (typeof name === 'string') {
-    assert(name.length <= 32, 'Name must be 32 characters or less');
-
-    Object.assign(patch, { name });
+  if (typeof mutation.name === 'string') {
+    assert(mutation.name.length <= 32, 'Name must be 32 characters or less');
   }
 
-  return patch;
+  return mutation;
 };
