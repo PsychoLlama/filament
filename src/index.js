2cc2870961e26594fa7bf0e476d2f54a7232057a
Jesse Gibson <Jesse_Gibson@me.com>
2018-01-04 19:52:12 -0700

    Improve status endpoint
    Return some metadata instead of the string "Online". Not a lot of
    metadata, just enough to distinguish it from any other service which
    might respond to an http request.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import graphqlHttp from 'express-graphql';
 import express from 'express';
-import schema from './schema';
 import rc from 'rc';
 
 import { createHueLoaders } from './context';
 import * as rootValue from './resolvers';
+import schema from './schema';
+import pkg from '../package';
 
 const config = rc('filament', {
   host: '0.0.0.0',
@@ -21,7 +22,11 @@ const graphqlEndpoint = graphqlHttp(() => ({
 
 const app = express();
 
-app.get('/status', (req, res) => res.status(200).end('Online'));
+app.get('/status', (req, res) => {
+  const { name: app, version } = pkg;
+  res.status(200).json({ version, app });
+});
+
 app.use(graphqlEndpoint);
 
 app.listen(config.port, config.host);
